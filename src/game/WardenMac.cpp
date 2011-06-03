ec277a436018e0a77144fa857ad5460f138cf745
TOM_RUS <tom_rus@getmangos.com>
2011-06-04 01:07:19 +0400

    More mac fixes.

--- src/game/WardenMac.cpp
+++ src/game/WardenMac.cpp
@@ -47,10 +47,10 @@ void WardenMac::Init(WorldSession *pClient, BigNumber *K)
     WK.generate(OutputKey, 16);
     /*
     Seed: 4D808D2C77D905C41A6380EC08586AFE (0x05 packet)
-    Hash: 568C054C781A972A6037A2290C22B52571A06F4E (0x04 packet)
-    Module MD5: 79C0768D657977D697E10BAD956CCED1
-    New Client Key: 7F 96 EE FD A5 B6 3D 20 A4 DF 8E 00 CB F4 83 04
-    New Cerver Key: C2 B7 AD ED FC CC A9 C2 BF B3 F8 56 02 BA 80 9B
+    Hash: <?> (0x04 packet)
+    Module MD5: 0DBBF209A27B1E279A9FEC5C168A15F7
+    New Client Key: <?>
+    New Cerver Key: <?>
     */
     uint8 mod_seed[16] = { 0x4D, 0x80, 0x8D, 0x2C, 0x77, 0xD9, 0x05, 0xC4, 0x1A, 0x63, 0x80, 0xEC, 0x08, 0x58, 0x6A, 0xFE };
 
@@ -116,12 +116,41 @@ void WardenMac::RequestHash()
 
 void WardenMac::HandleHashResult(ByteBuffer &buff)
 {
+
+    // test
+    int keyIn[4];
+
+    uint8 mod_seed[16] = { 0x4D, 0x80, 0x8D, 0x2C, 0x77, 0xD9, 0x05, 0xC4, 0x1A, 0x63, 0x80, 0xEC, 0x08, 0x58, 0x6A, 0xFE };
+
+    for(int i = 0; i < 4; ++i)
+    {
+        keyIn[i] = *(int*)(&mod_seed[0] + i * 4);
+    }
+
+    int keyOut[4];
+    int keyIn1, keyIn2;
+    keyOut[0] = keyIn[0];
+    keyIn[0] ^= 0xDEADBEEFu;
+    keyIn1 = keyIn[1];
+    keyIn[1] -= 0x35014542u;
+    keyIn2 = keyIn[2];
+    keyIn[2] += 0x5313F22u;
+    keyIn[3] *= 0x1337F00Du;
+    keyOut[1] = keyIn1 - 0x6A028A84;
+    keyOut[2] = keyIn2 + 0xA627E44;
+    keyOut[3] = 0x1337F00D * keyIn[3];
+    // end test
+
     buff.rpos(buff.wpos());
 
-    const uint8 validHash[20] = { 0x56, 0x8C, 0x05, 0x4C, 0x78, 0x1A, 0x97, 0x2A, 0x60, 0x37, 0xA2, 0x29, 0x0C, 0x22, 0xB5, 0x25, 0x71, 0xA0, 0x6F, 0x4E };
+    Sha1Hash sha1;
+    sha1.UpdateData((uint8*)keyIn, 16);
+    sha1.Finalize();
+
+    //const uint8 validHash[20] = { 0x56, 0x8C, 0x05, 0x4C, 0x78, 0x1A, 0x97, 0x2A, 0x60, 0x37, 0xA2, 0x29, 0x0C, 0x22, 0xB5, 0x25, 0x71, 0xA0, 0x6F, 0x4E };
 
     // verify key not equal kick player
-    if (memcmp(buff.contents() + 1, validHash, sizeof(validHash)) != 0)
+    if (memcmp(buff.contents() + 1, sha1.GetDigest(), 20) != 0)
     {
         sLog.outWarden("Request hash reply: failed");
         if (sWorld.getConfig(CONFIG_BOOL_WARDEN_KICK))
@@ -132,14 +161,14 @@ void WardenMac::HandleHashResult(ByteBuffer &buff)
     sLog.outWarden("Request hash reply: succeed");
 
     // client 7F96EEFDA5B63D20A4DF8E00CBF48304
-    const uint8 client_key[16] = { 0x7F, 0x96, 0xEE, 0xFD, 0xA5, 0xB6, 0x3D, 0x20, 0xA4, 0xDF, 0x8E, 0x00, 0xCB, 0xF4, 0x83, 0x04 };
+    //const uint8 client_key[16] = { 0x7F, 0x96, 0xEE, 0xFD, 0xA5, 0xB6, 0x3D, 0x20, 0xA4, 0xDF, 0x8E, 0x00, 0xCB, 0xF4, 0x83, 0x04 };
 
     // server C2B7ADEDFCCCA9C2BFB3F85602BA809B
-    const uint8 server_key[16] = { 0xC2, 0xB7, 0xAD, 0xED, 0xFC, 0xCC, 0xA9, 0xC2, 0xBF, 0xB3, 0xF8, 0x56, 0x02, 0xBA, 0x80, 0x9B };
+    //const uint8 server_key[16] = { 0xC2, 0xB7, 0xAD, 0xED, 0xFC, 0xCC, 0xA9, 0xC2, 0xBF, 0xB3, 0xF8, 0x56, 0x02, 0xBA, 0x80, 0x9B };
 
     // change keys here
-    memcpy(InputKey, client_key, 16);
-    memcpy(OutputKey, server_key, 16);
+    memcpy(InputKey, keyIn, 16);
+    memcpy(OutputKey, keyOut, 16);
 
     iCrypto.Init(InputKey);
     oCrypto.Init(OutputKey);
@@ -155,7 +184,12 @@ void WardenMac::RequestData()
 
     ByteBuffer buff;
     buff << uint8(WARDEN_SMSG_CHEAT_CHECKS_REQUEST);
-    // TODO: Implement real data
+
+    std::string str = "Test string!";
+
+    buff << uint8(str.size());
+    buff.append(str.c_str(), str.size());
+
     buff.hexlike();
 
     // Encrypt with warden RC4 key.
@@ -175,20 +209,52 @@ void WardenMac::HandleData(ByteBuffer &buff)
     m_WardenDataSent = false;
     m_WardenKickTimer = 0;
 
-    uint16 Length;
-    buff >> Length;
-    uint32 Checksum;
-    buff >> Checksum;
+    //uint16 Length;
+    //buff >> Length;
+    //uint32 Checksum;
+    //buff >> Checksum;
 
-    if (!IsValidCheckSum(Checksum, buff.contents() + buff.rpos(), Length))
-    {
-        buff.rpos(buff.wpos());
-        if (sWorld.getConfig(CONFIG_BOOL_WARDEN_KICK))
-            Client->KickPlayer();
-        return;
-    }
+    //if (!IsValidCheckSum(Checksum, buff.contents() + buff.rpos(), Length))
+    //{
+    //    buff.rpos(buff.wpos());
+    //    if (sWorld.getConfig(CONFIG_BOOL_WARDEN_KICK))
+    //        Client->KickPlayer();
+    //    return;
+    //}
 
     bool found = false;
+
+    std::string str = "Test string!";
+
+    Sha1Hash sha1;
+    sha1.UpdateData(str);
+    uint32 magic = 0xFEEDFACE;                              // unsure
+    sha1.UpdateData((uint8*)&magic, 4);
+    sha1.Finalize();
+
+    uint8 sha1Hash[20];
+    buff.read(sha1Hash, 20);
+
+    if (memcmp(sha1Hash, sha1.GetDigest(), 20))
+    {
+        sLog.outWarden("Handle data failed: SHA1 hash is wrong!");
+        found = true;
+    }
+
+    MD5_CTX ctx;
+    MD5_Init(&ctx);
+    MD5_Update(&ctx, str.c_str(), str.size());
+    uint8 ourMD5Hash[16];
+    MD5_Final(ourMD5Hash, &ctx);
+
+    uint8 theirsMD5Hash[16];
+    buff.read(theirsMD5Hash, 16);
+
+    if (memcmp(ourMD5Hash, theirsMD5Hash, 16))
+    {
+        sLog.outWarden("Handle data failed: MD5 hash is wrong!");
+        found = true;
+    }
 
     if (found && sWorld.getConfig(CONFIG_BOOL_WARDEN_KICK))
         Client->KickPlayer();
